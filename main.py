6a058bc1c30e4e325a7aaf3e964b9bf26b612c58
Kayden <kaydengthompson@gmail.com>
2019-01-16 13:05:40 -0700

    finished scoring for individual songs, refactored to allow a full database scan without memory overload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 def create_connection(db_file):
     try:
         connect = sqlite3.connect(db_file)
@@ -47,28 +48,35 @@ def filter_details(conn):
         insert_statement = "insert into SongTable (userID, song, artist, details) values (?, ?, ?, ?);";
         conn.execute(insert_statement, (item[0], item[1], item[2], item[3]));
 
+    conn.commit()
+
 
 #re.sub(r'([^\s\w]|_)+', '', row[1])  only retains characters that are alphanum or whitespace
 
 # (\([^()]+\)) capture group with ( match anything that isnt ( or ) then )
 
+#compare one single playlist against another for similarity
 def find_similarity(source_list, compare_list):
+
+    #hash maps store (song, artist) tuples as keys
     src_dict = {}
     cmp_dict = {}
     main_dict = {}
 
     # store frequency of each term in corresponding dict. main is union of both
     for item in source_list:
-        if not item in src_dict:
-            src_dict[item] = 1
-        if not item in main_dict:
-            main_dict[item] = 1
+        pair = (item[1], item[2])
+        if not pair in src_dict:
+            src_dict[pair] = 1
+        if not pair in main_dict:
+            main_dict[pair] = 1
 
     for item in compare_list:
-        if not item in cmp_dict:
-            cmp_dict[item] = 1
-        if not item in main_dict:
-            main_dict[item] = 1
+        pair = (item[1], item[2])
+        if not pair in cmp_dict:
+            cmp_dict[pair] = 1
+        if not pair in main_dict:
+            main_dict[pair] = 1
 
     src_list = []
     cmp_list = []
@@ -84,72 +92,113 @@ def find_similarity(source_list, compare_list):
 
     src_array = np.array([src_list])
     cmp_array = np.array([cmp_list])
+    cs = cosine_similarity(cmp_array, src_array)[0][0]
+    return 100 * cs * cs / len(compare_list)
 
-    return cosine_similarity(cmp_array, src_array)[0][0]
 
-
+#perform a batch job of similarity comparisons between playlists, returns map of results
 def find_most_similar(source_list, compare_lists, quantity):
+    playlist_scores = {}
+    for i in range(0,len(compare_lists)):
+        playlist_scores[compare_lists[i][0][0]] = find_similarity(source_list, compare_lists[i])
+
+    return playlist_scores
+
+
+#compare playlists and display most similar to user in ranked order
+def show_similar_playlists(source_list, compare_lists, quantity):
     comparison_results = []
     #store tuples containing id and similarity score of each list to the source list
     for i in range(0,len(compare_lists)):
         comparison_results.append(
-            (i, find_similarity(source_list, compare_lists[i]))
-        );
-    return sorted(comparison_results, reverse =True, key=lambda x: x[1])
+            (compare_lists[i][0][0], find_similarity(source_list, compare_lists[i]))
+        )
+    comparison_results = sorted(comparison_results, reverse =True, key=lambda x: x[1])
 
-def main():
-    database ="/home/nebrya/PycharmProjects/musicpredict/songs.db";
-
-    # create a database connection
-    conn = create_connection(database)
-
-    #filter_details(conn)
+    print "similarity of ref_songs to temp_songs: "
+    for item in comparison_results[0:quantity]:
+        print "playlist ID:",item[0], "score:", item[1]
 
 
-    query = conn.execute("SELECT userID, song, artist FROM SongTable order by userID limit 500;");
-    query_list = list(query.fetchall())
+def get_best_songs(ref_playlist, cmp_playlists, quantity):
+    song_score = {}
+    scored_playlists = find_most_similar(ref_playlist, cmp_playlists, quantity)
+
+    #sum together the frequencies of each song multiplied by their respective score coefficient
+    # song score = (0.2653)1 + (0.7829)1 + ...
+    #so the songs which occur most frequently and in the most relevant playlists will float to the top
+
+    for playlist in cmp_playlists:
+        for song_obj in playlist:
+            pair =(song_obj[1],song_obj[2]) #(track, artist)
+            if pair in song_score:
+                song_score[pair] += scored_playlists[song_obj[0]]
+            else:
+                song_score[pair] = scored_playlists[song_obj[0]]
+
+    #filter out every song already provided in the source playlist
+    for song_obj in ref_playlist:
+        pair = (song_obj[1], song_obj[2])
+        if pair in song_score:
+            del song_score[pair]
+
+    best_songs = song_score.items()
+    return sorted(best_songs, reverse=True, key=lambda x: x[1])[0:quantity]
 
 
-    #group all rows into lists by userID
+#convert query object  into list, then make a list of sublists grouped by userID to create "playlists"
+def bundle_query_to_playlist(q):
+    query_list = list(q.fetchall())
+    # group all rows from database into lists by userID
     playlist_list = [[]]
     j = 0
     for i in range(0, len(query_list)):
         if i > 0:
-            if query_list[i][0] == query_list[i-1][0]:#if the userID has changed
-                playlist_list[j].append(query_list[i])#append next row into same list
-            else:#userID not matched
-                playlist_list.append([query_list[i]]) #begin new array in playlist[j]
+            if query_list[i][0] == query_list[i - 1][0]:  # if the userID has changed
+                playlist_list[j].append(query_list[i])  # append next row into same list
+            else:  # userID not matched
+                playlist_list.append([query_list[i]])  # begin new array in playlist[j]
                 j += 1
         else:
             playlist_list[j].append(query_list[i])
 
+    return playlist_list
 
-    #fixed_rows = []
-
-    #for row in query:
-    #    aa= row[0]
-
-    ref_songs = ["why", "should", "I", "cry", "when", "angels", "deserve", "to", "die"]
-    temp_songs = [["what", "is", "not", "wrong", "with", "this", "to", "die"],
-                  ["this","is","the","time"],
-                  ["where","should","we"],
-                  ["why","not"],
-                  ["what","the","fuck","is","going","on","right","here"]
-                 ]
-
-    print "similarity of ref_songs to temp_songs: "
-    for item in find_most_similar(playlist_list[1], playlist_list[2:], 5):
-        print "playlist ID:",item[0], "score:", item[1]
+def main():
+    database ="/home/nebrya/PycharmProjects/musicpredict/songs.db";
+    conn = create_connection(database)
+    #filter_details(conn)
 
 
-    conn.commit()
+    '''
+    query = conn.execute("SELECT userID, song, artist FROM SongTable order by userID limit 4000000;");
+    bulk_playlists = bundle_query_to_playlist(query)
+
+    query = conn.execute("SELECT userID, song, artist FROM SongTable where userID = 0;");
+    source_playlist = bundle_query_to_playlist(query)[0]
+    num_results = 20
+    show_similar_playlists(source_playlist, bulk_playlists, 20)
+    '''
+
+    #Memory efficient alternative for local testing
+    best_songs= []
+    num_results = 20
+    query = conn.execute("SELECT userID, song, artist FROM SongTable where userID = 0;");
+    source_playlist = bundle_query_to_playlist(query)[0]
+    # process 3 million rows at a time so memory does not crash
+    for i in range(0,96):
+        query = conn.execute("SELECT userID, song, artist FROM SongTable where userID > ? and userID <= ? order by userID;", (i*10000, (i+1)*10000));
+        print "checkpoint - finished round ", i
+        bulk_playlists = bundle_query_to_playlist(query)
+        best_songs += get_best_songs(source_playlist, bulk_playlists, num_results)
+
+    best_songs = sorted(best_songs, reverse=True, key=lambda x: x[1])[0:num_results*5]
+    for song_obj in best_songs:
+         print "SONG:", song_obj[0][0], "ARTIST:", song_obj[0][1], "SCORE:", song_obj[1]
 
 
     #TODO: attempt with database, iteratively pull small loads into memory and process scores
-    #ensure query either pulls one id at a time, or replace iterator id with database id
-
-    #once it works with entire database and finds most similar playlists ranked descending,
-    #tally up the most commmonly occurring songs out of the top n playlists, list those songs descending
+    #TODO:  write functions to find most recommended artist instead of song
 
 if __name__ == '__main__':
     main()
